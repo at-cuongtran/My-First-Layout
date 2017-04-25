dd068ed52789257d0d938828e026a8fa29733fbc
gt-cuongtran <cuong.tran.asiantech@gmail.com>
2017-04-25 16:26:29 +0700

    update quantity and delete

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,95 @@
 var Product = function ($name, $price, $quantity) {
 	this.name = $name;
 	this.price = $price;
-	this.quantity = $quantity;	
-}
-Product.prototype.sum = function() {
+	this.quantity = $quantity;
+};
+
+Product.prototype.total = function() {
 	return this.price * this.quantity;
 };
 
-var pen = new Product("Pen", 5, 100);
-var pencil = new Product("Pencil", 10, 150);
+var products = [];
+var pen = new Product('Pen', 5, 4);
+products.push(pen);
+var pencil = new Product('Pencil', 10, 8);
+products.push(pencil);
 
-
-function createRow($className, $table) {
-	var tr = document.createElement('tr');
-	tr.setAttribute("class", $className);
-
-	$table.appendChild(tr);
+function newElement($element, $attribute) {
+	var element = document.createElement($element);
+	for (var attr in $attribute){
+		element.setAttribute(attr, $attribute[attr]);
+	}
+	return element;
 }
 
-function createHead($content, $row) {
-	var th = document.createElement('th');
-	var content = document.createTextNode($content);
-	th.appendChild(content);
-
-	$row.appendChild(th);
+function updateTotal() {
+	var sum = 0;
+	for (var id in products) {
+		var selectedValue = parseInt(selectElement[id].value);
+		products[id].quantity = selectedValue;
+		var total = products[id].total();
+		table.tBodies[0].rows[id].cells[4].innerHTML = total;
+		sum += total;
+	}
+	table.tFoot.rows[0].cells[1].innerHTML = sum;
 }
 
-function createCell($content, $row) {
-	var td = document.createElement('td');
-	var content = document.createTextNode($content);
-	td.appendChild(content);
+var container = document.getElementsByClassName('container');
+var selectElement = document.getElementsByClassName('select-list');
+var table = newElement('table', {'class':'table'});
 
-	$row.appendChild(td);
+// delete curtain product in products
+function deleteProduct(elt) {
+	var parentElement = elt.parentNode.parentNode;
+	products.splice(parentElement.cells[0].innerHTML, 1);
+	parentElement.remove();
+	console.log(parentElement);
+	updateTotal();
 }
 
-// Create table element with class
-var table = document.createElement("table");
-table.setAttribute("class", "table");
+// CREATE SELECT LIST STRING
+var selectString = '<select class="select-list" onChange="updateTotal()">';
+for (var i = 0; i <= 10; i++) {
+	selectString += '<option value="' + i + '">' + i + '</option>';
+}
+selectString += '</select>';
 
-// Insert table into demo div
-var outerElement = document.getElementsByClassName("demo");
-outerElement[0].appendChild(table);
+// Create Table String
+var sum = 0;
+var contentString = '';
+var headString = '<thead><th>Index</th><th>Name</th><th>Price</th><th>Quantity</th><th>total</th></thead>';
+var bodyString = '<tbody>';
+for (var id in products) {
+	var total = products[id].total();
+	sum += total;
+	bodyString += '<tr>';
+	bodyString += '<td>' + id + '</td>';
+	bodyString += '<td>' + products[id].name + '</td>';
+	bodyString += '<td>' + products[id].price + '</td>';
+	bodyString += '<td>' + selectString + '</td>';
+	bodyString += '<td>' + total + '</td>';
+	bodyString += '<td><input type="button" value="Delete" onClick="deleteProduct(this)"></td>';
+	bodyString += '</tr>';
+}
+bodyString += '</tbody>';
 
-// Insert a row into table
-var tableElement = document.getElementsByClassName("table");
-createRow("row",tableElement[0]);
+var footString = '<tfoot>';
+footString += '<tr>';
+footString += '<td colspan="4">sum</td><td>' + sum + '</td>';
+footString += '</tr>';
+footString += '</tfoot>';
 
-// Insert table head
-var rowElement = document.getElementsByClassName("row");
-createHead("name", rowElement[0]);
-createHead("price", rowElement[0]);
-createHead("quantity", rowElement[0]);
-createHead("sum", rowElement[0]);
+contentString += headString + bodyString + footString;
+table.innerHTML = contentString;
+container[0].appendChild(table);
 
-// Insert data 
-createRow("row",tableElement[0]);
-createCell(pen.name, rowElement[1]);
-createCell(pen.price, rowElement[1]);
-createCell(pen.quantity, rowElement[1]);
-createCell(pen.sum(), rowElement[1]);
-
-// Insert data 
-createRow("row",tableElement[0]);
-createCell(pencil.name, rowElement[2]);
-createCell(pencil.price, rowElement[2]);
-createCell(pencil.quantity, rowElement[2]);
-createCell(pencil.sum(), rowElement[2]);
-
-
-
-
-console.log("hello!");
+// CHANGE QUANTITY VALUE MATCH PRODUCT.QUANTITY
+for (var id in products){
+	for (var nodeId = 0; nodeId <= 10; nodeId++){
+		var optionElement = selectElement[id].getElementsByTagName('option')[nodeId];
+		if (optionElement.value == products[id].quantity){
+			optionElement.setAttribute('selected', 'selected');
+			break;
+		}
+	}
+}
